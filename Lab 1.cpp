16536d97fd65b9c13be1b3dbf82f8bf8b7a64298
ThfoBa <danilstarikov03@gmail.com>
2022-09-17 17:39:43 +0700

    Add func "main" without dynamic variable

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -29,31 +29,39 @@ pitomnik initialization(int k, double p, double r, int a, int v, int h, int pr)
 	check.kolvo = k;
 	check.pribil = p;
 	check.rasxodi = r;
+	check.price_korm = 15;
+	strcpy_s(check.dogs.name, default_pets_name);
 	check.dogs.age = a;
 	check.dogs.ves = v;
 	check.dogs.health = h;
 	check.dogs.price = pr;
-	check.price_korm = 15;
 	return check;
 }
 
-pitomnik vvod(pitomnik tmp)
+pitomnik vvod()
 {
-	
+	pitomnik tmp;
 	tmp = initialization(0, 0, 0, 0, 0, 0, 0);
 	printf("Введите имя животного: \n");
 	rewind(stdin);
 	gets_s(tmp.dogs.name, LN);
 	printf("Введите возраст животного (кол-во полных месяцев): \n");
-	scanf("%d", &tmp.dogs.age);
+	scanf_s("%d", &tmp.dogs.age);
 	printf("Введите вес животного (в граммах): \n");
-	scanf("%d", &tmp.dogs.ves);
+	scanf_s("%d", &tmp.dogs.ves);
 	printf("Введите состояние здоровья животного (от 1 до 10): \n");
-	scanf("%d", &tmp.dogs.health);
+	scanf_s("%d", &tmp.dogs.health);
 	printf("Введите стоимость животного: \n");
-	scanf("%lf", &tmp.dogs.price);
+	scanf_s("%lf", &tmp.dogs.price);
 	tmp.kolvo++;
 	return tmp;
+}
+
+void add_price(pitomnik dog1, pitomnik dog2)
+{
+	double sum;
+	sum = dog1.dogs.price + dog2.dogs.price;
+	printf("Итоговая стоимость животных: %lf руб.\n", sum);
 }
 
 void vivod(pitomnik tmp)
@@ -71,7 +79,7 @@ void vivod(pitomnik tmp)
 pitomnik kormlenie(pitomnik tmp)
 {
 	tmp.dogs.ves += 1;
-	tmp.rasxodi = tmp.rasxodi + tmp.price_korm * tmp.kolvo;
+	tmp.rasxodi = tmp.rasxodi + (tmp.price_korm * tmp.kolvo);
 	printf("Животные накормлены!\n");
 	return tmp;
 }
@@ -79,7 +87,7 @@ pitomnik prodaja(pitomnik tmp)
 {
 	tmp.kolvo -= 1;
 	tmp.pribil += tmp.dogs.price;
-	tmp.rasxodi -= tmp.price_korm * tmp.kolvo;
+	tmp.rasxodi -= (tmp.price_korm * tmp.kolvo);
 	strcpy_s(tmp.dogs.name, default_pets_name);
 	tmp.dogs.age = 0;
 	tmp.dogs.ves = 0;
@@ -88,3 +96,28 @@ pitomnik prodaja(pitomnik tmp)
 	printf("Животное продано!\n");
 	return tmp;
 }
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	pitomnik tmp;
+	int kolvo = 0;
+	tmp = initialization(kolvo, 0, 0, 0, 0, 0, 0);
+	printf("Инициализация: \n");
+	vivod(tmp);
+	tmp = vvod();
+	printf("\nПосле ввода: \n");
+	vivod(tmp);
+	
+	tmp = kormlenie(tmp);
+	printf("\nПосле кормления: \n");
+	vivod(tmp);
+	tmp = prodaja(tmp);
+	printf("\n После продажи: \n");
+	vivod(tmp);
+	
+	
+	return 0;
+}
+
+		
