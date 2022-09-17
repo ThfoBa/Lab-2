d962c0a624d8acac460f4f73115af77693bd11ed
ThfoBa <danilstarikov03@gmail.com>
2022-09-17 16:14:06 +0700

    Add structs

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -1,1 +1,24 @@
-﻿
+﻿// Разработка программного модуля, описывающего работу питомника для собак (структуры языка C) 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <locale.h>
+#define LN 100
+#define default_pets_name "no_name"
+
+typedef struct {
+	char name[LN];
+	int age;
+	int ves;
+	int health;
+	double price;
+}pets;
+
+typedef struct {
+	int kolvo;
+	double pribil;
+	double rasxodi;
+	int price_korm;
+	pets dogs;
+}pitomnik;
