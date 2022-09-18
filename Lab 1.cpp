b1c01f25dce764246988c1c6dd6acba6b13d657f
ThfoBa <danilstarikov03@gmail.com>
2022-09-18 16:25:03 +0700

    Add class "pets"

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -74,3 +74,117 @@ private:
 	double rasxodi;
 	int price_kormlenie;
 };
+class pets : public pitomnik
+{
+	public: 
+		pets()
+		{
+			strcpy_s(name, default_pets_name);
+			age = 0;
+			ves = 0;
+			health = 0;
+			price = 0;
+		}
+		pets(int kolvo, double pribil, double rasxodi, int price_kormlenie, char name[LN], int age, int ves, int health, double price) : pitomnik(kolvo, pribil, rasxodi, price_kormlenie)
+		{
+			strcpy_s(this->name, name);
+			this->age = age;
+			this->ves = ves;
+			this->health = health;
+			this->price = price;
+		}
+		void setname(char name[LN])
+		{
+			strcpy_s(this->name, name);
+		}
+		void setage(int age)
+		{
+			this->age = age;
+		}
+		void setves(int ves)
+		{
+			this->ves = ves;
+		}
+		void sethealth(int health)
+		{
+			this->health = health;
+		}
+		void setprice(double price)
+		{
+			this->price = price;
+		}
+		string getname()
+		{
+			cout << "Имя животного: " << name << endl;
+			return name;
+		}
+		int getage()
+		{
+			cout << "Возраст животного (кол-во полных месяцев): " << age << endl;
+			return age;
+		}
+		int getves()
+		{
+			cout << "Вес животного (в граммах): " << ves << endl;
+			return ves;
+		}
+		void vvod()
+		{
+			cout << "Введите имя животного: \n";
+			rewind(stdin); gets_s(name, LN);
+			cout << "Введите возраст животного (кол-во полных месяцев): \n";
+			cin >> age;
+			cout << "Введите вес животного (в граммах): \n";
+			cin >> ves;
+			cout << "Введите состояние здоровья животного (целое число от 1 до 10): \n";
+			cin >> health;
+			cout << "Введите стоимость животного: \n";
+			cin >> price;
+			kolvo++;
+		}
+		void vivod()
+		{
+			cout << "Количество животных: " << kolvo << endl;
+			cout << "Прибыль питомника: " << pribil << endl;
+			cout << "Расходы питомника: " << rasxodi << endl;
+			cout << "Стоимость корма для собак: " << price_kormlenie << endl;
+			cout << "Имя животного: " << name << endl;
+			cout << "Возраст животного: " << age << endl;
+			cout << "Вес животного: " << ves << endl;
+			cout << "Состояние здоровья животного: " << health << endl;
+			cout << "Стоимость животного: " << price << endl;
+		}
+		void kormlenie()
+		{
+			ves += 1;	
+			rasxodi = rasxodi + price_kormlenie;
+			cout << "Животное накормлено!\n";
+		}
+		void prodaja()
+		{
+			pribil += price;
+			kolvo -= 1;
+			rasxodi -= price_kormlenie;
+			strcpy_s(name, default_pets_name);
+			age = 0;
+			ves = 0;
+			health = 0;
+			price = 0;
+			cout << "Животное продано!\n";
+		}
+		double add_price(double sum)
+		{
+			sum += price;
+			cout << "Итоговая стоимость животных: " << sum << endl;
+			return sum;
+		}
+	private:
+		char name[LN];
+		int age;
+		int ves;
+		int health;
+		double price;
+};
+
+
+
