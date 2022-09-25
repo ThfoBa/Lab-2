507d7a47377a8ffc29edc8a716fb3bb917e85dab
ThfoBa <danilstarikov03@gmail.com>
2022-09-25 20:40:50 +0700

    Изменил всё

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -3,11 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
+#include <conio.h>
 #include <locale.h>
 #define LN 100
-#define default_pets_name "no_name"
+#define default_pets_name "Дружок"
 
 typedef struct {
+	int id; 
 	char name[LN];
 	int age;
 	int ves;
@@ -20,124 +22,172 @@ typedef struct {
 	double pribil;
 	double rasxodi;
 	int price_korm;
-	pets dogs;
+	pets dogs[LN];
 }pitomnik;
 
-pitomnik initialization(int k, double p, double r, int a, int v, int h, int pr)
+
+pitomnik initialization(int k, double p, double r,int id, int a, int v, int h, int pr)
 {
-	pitomnik check; // вспомогательная переменная
+	pitomnik check; 
 	check.kolvo = k;
 	check.pribil = p;
 	check.rasxodi = r;
 	check.price_korm = 15;
-	strcpy_s(check.dogs.name, default_pets_name);
-	check.dogs.age = a;
-	check.dogs.ves = v;
-	check.dogs.health = h;
-	check.dogs.price = pr;
+	for (int i = 0; i < k; i++)
+	{
+		check.dogs[i].id = id;
+		strcpy_s(check.dogs[i].name, default_pets_name);
+		check.dogs[i].age = a;
+		check.dogs[i].ves = v;
+		check.dogs[i].health = h;
+		check.dogs[i].price = pr;
+	}
 	return check;
 }
-
 pitomnik vvod()
 {
 	pitomnik tmp;
-	tmp = initialization(0, 0, 0, 0, 0, 0, 0);
-	printf("Введите имя животного: \n");
-	rewind(stdin);
-	gets_s(tmp.dogs.name, LN);
-	printf("Введите возраст животного (кол-во полных месяцев): \n");
-	scanf_s("%d", &tmp.dogs.age);
-	printf("Введите вес животного (в граммах): \n");
-	scanf_s("%d", &tmp.dogs.ves);
-	printf("Введите состояние здоровья животного (от 1 до 10): \n");
-	scanf_s("%d", &tmp.dogs.health);
-	printf("Введите стоимость животного: \n");
-	scanf_s("%lf", &tmp.dogs.price);
-	tmp.kolvo++;
+	int kol;
+	tmp = initialization(1, 0, 0, 27, 23, 2000, 10,999);
+	printf("Введите количество животных, которых требуется добавить: \n");
+	scanf_s("%d", &kol);
+	for (int i = 1; i <= kol; i++)
+	{
+		printf("\nID: \n");
+		scanf_s("%d", &tmp.dogs[i].id);
+		printf("Введите имя животного: \n");
+		rewind(stdin);
+		gets_s(tmp.dogs[i].name, LN);
+		printf("Введите возраст животного (кол-во полных месяцев): \n");
+		scanf_s("%d", &tmp.dogs[i].age);
+		printf("Введите вес животного (в граммах): \n");
+		scanf_s("%d", &tmp.dogs[i].ves);
+		printf("Введите состояние здоровья животного (от 1 до 10): \n");
+		scanf_s("%d", &tmp.dogs[i].health);
+		printf("Введите стоимость животного: \n");
+		scanf_s("%lf", &tmp.dogs[i].price);
+	}
+	tmp.kolvo += kol;
 	return tmp;
 }
-
-void add_price(pitomnik dog1, pitomnik dog2)
+void add_price(pitomnik tmp)
 {
-	double sum;
-	sum = dog1.dogs.price + dog2.dogs.price;
+	double sum = 0;
+
+	for (int i = 0; i < (tmp.kolvo); i++)
+	{
+		sum += tmp.dogs[i].price;
+	}
+
 	printf("Итоговая стоимость животных: %lf руб.\n", sum);
 }
-
 void vivod(pitomnik tmp)
 {
+
 	printf("Количество животных: %d шт.\n", tmp.kolvo);
 	printf("Прибыль питомника: %lf руб.\n", tmp.pribil);
 	printf("Расходы питомника: %lf руб.\n", tmp.rasxodi);
 	printf("Стоимость корма: %d руб.\n", tmp.price_korm);
-	printf("Имя животного: %s\n", tmp.dogs.name);
-	printf("Возраст животного: %d мес.\n", tmp.dogs.age);
-	printf("Вес животного: %d гр.\n", tmp.dogs.ves);
-	printf("Состояние здоровья животного: %d\n", tmp.dogs.health);
-	printf("Стоимость животного: %lf руб.\n", tmp.dogs.price);
+	for (int i = 0; i < (tmp.kolvo); i++)
+	{
+		printf("\nID: %d\n", tmp.dogs[i].id);
+		printf("Имя животного: %s\n", tmp.dogs[i].name);
+		printf("Возраст животного: %d мес.\n", tmp.dogs[i].age);
+		printf("Вес животного: %d гр.\n", tmp.dogs[i].ves);
+		printf("Состояние здоровья животного: %d\n", tmp.dogs[i].health);
+		printf("Стоимость животного: %lf руб.\n", tmp.dogs[i].price);
+	}
 }
 pitomnik kormlenie(pitomnik tmp)
 {
-	tmp.dogs.ves += 1;
+	for (int i = 0; i < tmp.kolvo; i++)
+	{
+		tmp.dogs[i].ves += 1;
+
+	}
 	tmp.rasxodi = tmp.rasxodi + (tmp.price_korm * tmp.kolvo);
 	printf("Животные накормлены!\n");
 	return tmp;
 }
 pitomnik prodaja(pitomnik tmp)
 {
+	int i,id,k=0;
+	printf("Введите ID животного, которое требуется продать: ");
+	scanf_s("%d", &id);
+	for (int i = 0; i < (tmp.kolvo); i++)
+	{
+		if (id == (tmp.dogs[i].id))
+		{
+			k = i;
+			break;
+		}
+	}
+	tmp.pribil += tmp.dogs[k].price;
+	if ((k != 0) && (k != ((tmp.kolvo) - 1)) && (id == (tmp.dogs[k].id)))
+	{
+		for (int i = k; i < ((tmp.kolvo) - 1); i++)
+		{
+			tmp.dogs[i] = tmp.dogs[i + 1];
+		}
+	}
+	else if ((k == 0) && (id == (tmp.dogs[k].id)))
+	{
+		for (int i = 0; i < ((tmp.kolvo) - 1); i++)
+		{
+			tmp.dogs[i] = tmp.dogs[i + 1];
+		}
+	}
+	else if ((id == (tmp.dogs[k].id))&& (k == ((tmp.kolvo) - 1)))
+	{
+		tmp.kolvo -= 1;
+	}
 	tmp.kolvo -= 1;
-	tmp.pribil += tmp.dogs.price;
-	tmp.rasxodi -= (tmp.price_korm * tmp.kolvo);
-	strcpy_s(tmp.dogs.name, default_pets_name);
-	tmp.dogs.age = 0;
-	tmp.dogs.ves = 0;
-	tmp.dogs.health = 0;
-	tmp.dogs.price = 0;
 	printf("Животное продано!\n");
 	return tmp;
 }
-
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	pitomnik tmp;
-	int kolvo = 0;
-	tmp = initialization(kolvo, 0, 0, 0, 0, 0, 0);
-	printf("Инициализация: \n");
-	vivod(tmp);
-	tmp = vvod();
-	printf("\nПосле ввода: \n");
-	vivod(tmp);
-	tmp = kormlenie(tmp);
-	printf("\nПосле кормления: \n");
-	vivod(tmp);
-	tmp = prodaja(tmp);
-	printf("\n После продажи: \n");
-	vivod(tmp);
-	pitomnik dog1;
-	dog1 = vvod();
-	add_price(tmp, dog1);
-	pitomnik* c = (pitomnik*)malloc(LN * sizeof(pitomnik));
-	printf("\nРабота с динамическими переменными:\n");
-	*c = initialization(kolvo, 0, 0, 0, 0, 0, 0);
-	printf("\nИнициализация: \n");
-	vivod(*c);
-	*c = vvod();
-	printf("\nПосле ввода: \n");
-	vivod(*c);
-	*c = kormlenie(*c);
-	printf("\nПосле кормления:\n");
-	vivod(*c);
-	*c = prodaja(*c);
-	printf("\nПосле продажи:\n");
-	vivod(*c);
-	pitomnik* c1 = (pitomnik*)malloc(LN * sizeof(pitomnik));
-	*c1 = vvod();
-	add_price(*c, *c1);
-	printf("\nАдреса динамических переменных:\n");
-	printf("%p", c);
+	char l;
+	do {
+		tmp = initialization(2, 0, 0, 27, 23, 2000, 10, 999);
+		vivod(tmp);
+		tmp = vvod();
+		printf("\nПосле ввода: \n");
+		vivod(tmp);
+		add_price(tmp);
+		tmp = kormlenie(tmp);
+		printf("\nПосле кормления: \n");
+		vivod(tmp);
+		tmp = prodaja(tmp);
+		printf("\n После продажи: \n");
+		vivod(tmp);
+		add_price(tmp);
+		printf("Для повтора программы нажмите любую клавишу.\n");
+		printf("Для выхода из программы нажмите ESC.\n");
+		l = _getch();
+	} while (l != 27);
 	return 0;
 }
+
+	
+	
+
+
+
+
+
+	
+	
+	
+	
+
+
+		
+
+		
+
 
 	
 	
